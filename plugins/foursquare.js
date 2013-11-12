74e69c54c33386155e446f1246c128cc3426814d
Tom Moor <tom.moor@gmail.com>
2013-11-12 23:04:23 +0000

    first commit

---
+++ plugins/foursquare.js
@@ -0,0 +1,10 @@
+var fsq = {};
+
+fsq.name = 'Foursquare';
+fsq.config = [{
+    name: 'auth_token',
+    label: 'Auth Token',
+    description: 'Create an app and paste the auth token here'
+}];
+
+module.exports = fsq;
