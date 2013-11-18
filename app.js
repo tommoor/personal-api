0d8cc2c7345d2984dc438ec630ece720fc9078be
Tom Moor <tom.moor@gmail.com>
2013-11-18 00:17:45 +0000

    Setup scripts continued

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs  = require('express3-handlebars');
+var fs = require('fs');
 var _ = require('underscore');
 var plugins = require('./plugins.js');
 var app = express();
@@ -8,27 +9,64 @@ var background = require('./background');
 var port = 5000;
 
 // background data fetching
-var job = new cronJob('* * * * * *', background.fetchAll, null, true);
+//var job = new cronJob('* * * * * *', background.fetchAll, null, true);
 
 // UI
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+app.get('/', function(req, res){
+    
+    fs.exists('config.js', function(err, exists){
+        
+        if (exists) {
+            res.redirect('/install');
+        } else {
+            res.redirect('/setup');
+        }
+    });
+});
+
+app.get('/install', function(req, res){
+    res.render('install');
+});
+
+app.post('/install', function(req, res){
+    
+    // test database config
+    var pg = require('pg');
+    var client = new pg.Client(req.body);
+    
+    client.connect(function(err) {
+      if (err) {
+        return console.error('could not connect to postgres', err);
+      }
+      
+      // write our db config file
+      fs.write('config.js', JSON.stringify(req.body), function(err, success){
+          
+      });
+    });
+    
+    res.render('install');
+});
 
 app.get('/setup', function(req, res){
+    
     plugins.getInstalled(function(err, response){
-        
         response = _.map(response, function(name){
-            return {
-                id: name,
-                plugin: require(name)
-            }
+            return {id: name, plugin: require(name)}
         });
-        
+
         res.render('setup', {
             plugins: response
         });
     });
 });
+
 
 app.post('/setup', function(req, res){
     
